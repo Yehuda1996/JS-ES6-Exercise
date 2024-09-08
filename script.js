5e7edb98bced1e556edc930aeccdaa6fb7e216e0
Yehuda Grajower <yehudag@Yehudas-MacBook-Pro.local>
2024-09-08 12:49:10 +0300

    added question 2

--- script.js
+++ script.js
@@ -5,4 +5,21 @@ function printArrow(num, direction){
     direction ? console.log(line + arrow) : console.log(arrow + line)
 }
 //2
+function printNums(){
+    let num = prompt("Enter a number: ");
+    let arr = [];
+
+    while (num != 0)
+    {
+        arr.push(num)
+    }
+    console.log(Math.max(arr));
+    console.log(arr.length);
+    const sum = arr.reduce((acc, curr) =>
+    acc + curr, 0)
+    console.log(sum)
+    const avg = sum / arr.length
+    console.log(avg)
+    arr.length > 4 ? console.log(arr.findIndex(4)) : console.log(arr.findIndex(arr.length - 1))
+}
 
